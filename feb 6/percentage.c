5b5a12bfb98cf00197624343cb4c61f9026ebdbd
Fazle Rabbi Naim <mdfazlerabbi3278@gmail.com>
2025-02-07 16:39:46 +0600

    Add files via upload

---
+++ feb 6/percentage.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float m1=95; //marks of math
+    float m2=94 ;//marks of physics
+    float m3=84 ;//marks of chemistry
+    float m4=85 ;//marks of biology
+    float m5=94 ;//marks of economics
+    float P=(m1+m2+m3+m4+m5)/5;
+    printf ("Percentage of 5 subject is:%f",P);
+    return 0;
+
+}
